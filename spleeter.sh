83c137984c32a8450784f1478bda1b3d05f6acba
TomScotch <TomScotch@web.de>
2019-12-15 09:08:56 +0100

    1576397336

--- spleeter.sh
+++ spleeter.sh
@@ -1,1 +1,1 @@
-for x in $( ls -1 $1/*.mp3 ) ; do spleeter separate -i $x -o $1 -p spleeter:4stems ; done
+for x in $( ls -1 $1/*.wav ) ; do spleeter separate -i $x -o $1/ -p spleeter:4stems ; done
--- split.sh
+++ split.sh
@@ -1,1 +1,1 @@
-mkdir $2 ; ffmpeg -i $1 -f segment -segment_time 60 -c copy $2/%03d.mp3
+mkdir $2 ; ffmpeg -i $1 -f segment -segment_time 60 -c copy $2/%03d.wav
