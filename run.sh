6409d78aad4edd52a8e4949d65e8a69f3f918be0
TomScotch <TomScotch@web.de>
2019-12-21 00:51:06 +0100

    1576885866

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
-for x in $( ls *.wav -1 ) ; do sh split.sh $x _$x/ ; done
+for x in $( ls *.wav -1 ) ; do sh split.sh $x ; done
 
 for x in $( ls _* -d -1 ) ; do sh spleeter.sh $x ; done
 
 for x in $( ls _* -d -1 ) ; do sh join.sh $x ; done
+
+for x in $( ls _* -d -1 ) ; do sh mix.sh $x ; done
--- split.sh
+++ split.sh
@@ -1,1 +1,1 @@
-mkdir $2 ; ffmpeg -i $1 -f segment -segment_time 60 -c copy $2/%03d.wav
+mkdir _$1 ; ffmpeg -i $1 -f segment -segment_time 60 -c copy _$1/%03d.wav
