e0c7e82575b3de38711dbef53ea226b5e8172ad3
tomscotch <tomscotch@web.de>
2020-02-20 09:45:41 +0100

    1582188341

--- spleeter.sh
+++ spleeter.sh
@@ -1,1 +1,1 @@
-for x in $( ls -1 $1/*.wav ) ; do spleeter separate -i $x -o $1/ -p spleeter:4stems  ; rm $x ; done
+for x in $( ls -1 $1/*.wav ) ; do spleeter separate -i $x -o $1/ -p spleeter:5stems  ; rm $x ; done
