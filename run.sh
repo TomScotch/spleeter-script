880b08737ed6700b979b677f9f2f32818826dd81
tomscotch <tomscotch@web.de>
2020-10-26 09:26:24 +0100

    1603700784

--- run.sh
+++ run.sh
@@ -1,18 +1,26 @@
-youtube-dl -f best -x $1 ;
+conda init bash ; 
+
+conda activate spleeter ;
+
+# sudo apt update ; sudo apt install ffmpeg shntool -y ;
+
+mkdir $1 ; cd $1 ;
+
+youtube-dl -f best -x $2 ;
 
 detox *.* ;
 
 for x in $( ls *.m4a -1 ) ; do ffmpeg -i $x $x.wav ; rm $x  ; done
 
-for x in $( ls *.wav -1 ) ; do sh split.sh $x ; done
+for x in $( ls *.wav -1 ) ; do sh ../split.sh $x ; done
 
-for x in $( ls _* -d -1 ) ; do sh spleeter.sh $x ; done
+for x in $( ls _* -d -1 ) ; do sh ../spleeter.sh $x ; done
 
-for x in $( ls _* -d -1 ) ; do sh join.sh $x ; done
+for x in $( ls _* -d -1 ) ; do sh ../join.sh $x ; done
 
-for x in $( ls _* -d -1 ) ; do sh mix.sh $x ; done
+for x in $( ls _* -d -1 ) ; do sh ../mix.sh $x ; done
 
-for x in $( ls _*/*.* -1 ) ; do sh convert.sh $x ; done
+for x in $( ls _*/*.* -1 ) ; do sh ../convert.sh $x ; done
 
 for x in $( ls _*/0* -1 -d ) ; do rm -rdf $x ; done
 
