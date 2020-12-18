b4eacdd1dcc41aebc2553ab31f1e2e9f21d647b2
tomscotch <tomscotch@web.de>
2020-12-18 02:43:59 +0100

    1608255839

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-# sudo apt install ffmpeg shntool -y ;
 
 mkdir $1 ; cd $1 ;
 
@@ -6,9 +5,11 @@ youtube-dl -q -f best -x $2 ;
 
 detox *.* ;
 
-for x in $( ls *.m4a -1 ) ; do ffmpeg -i $x  -b:a 256k $x.wav ; rm $x  ; done
+for x in $( ls *.m4a -1 ) ; do ffmpeg -i $x  $x.wav ; rm $x  ; done
 
-for x in $( ls *.wav -1 ) ; do sh ../split.sh $x ; done
+for x in $( ls *.* -1 ) ; do sh ../split.sh $x ; done
+
+for x in $( ls _*/*/*.* -1 ) ; do sh ../convert.sh $x ; done
 
 for x in $( ls _* -d -1 ) ; do sh ../spleeter.sh $x ; done
 
@@ -16,7 +17,7 @@ for x in $( ls _* -d -1 ) ; do sh ../join.sh $x ; done
 
 for x in $( ls _* -d -1 ) ; do sh ../mix.sh $x ; done
 
-for x in $( ls _*/*.* -1 ) ; do sh ../convert.sh $x ; done
+#for x in $( ls _*/mix*.* -1 ) ; do sh ../convert.sh $x ; done
 
 for x in $( ls _*/0* -1 -d ) ; do rm -rdf $x ; done
 
